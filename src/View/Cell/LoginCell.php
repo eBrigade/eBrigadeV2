2bd2ca78a571ff8f5db258270cb95b3163ba8ab2
Florent Maillard <florent.maillard.pro@gmail.com>
2016-08-30 15:10:01 +0200

    Ajout de la cell.

---
+++ src/View/Cell/LoginCell.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\View\Cell;
+
+use Cake\View\Cell;
+
+/**
+ * Login cell
+ */
+class LoginCell extends Cell
+{
+
+    /**
+     * List of valid options that can be passed into this
+     * cell's constructor.
+     *
+     * @var array
+     */
+    protected $_validCellOptions = [];
+
+    /**
+     * Default display method.
+     *
+     * @return void
+     */
+    public function display()
+    {
+    }
+}
