2687a3ea7d7a03facff1c83a9b573d8b17dfc1d7
Nirgalz <znirgal@gmail.com>
2016-09-24 09:01:11 +0200

    fix sur la query des users dispos, cette fois c est la bonne !

--- src/Controller/OperationsController.php
+++ src/Controller/OperationsController.php
@@ -253,7 +253,7 @@ class OperationsController extends AppController
                     return $q->distinct()->where(
                         ['OR' => [
 
-                            ['Users.id NOT IN ' => 'TeamsUsers.user_id'],
+                            ['Users.id IN ' => 'TeamsUsers.user_id'],
                             [function ($time) use ($timestart, $timeend) {
                                 return $time->between('Events.event_end_date', $timestart, $timeend, 'datetime');
                             }],
@@ -368,8 +368,46 @@ class OperationsController extends AppController
 
         //add or remove : link/unlink
         $action = $this->request->data('action');
-        debug($action);
-        $this->Gestion->ajoints($containerType, $contentType, $containerID, $contentID, $action);
+
+        //todo:
+        //component version
+        //$this->Gestion->ajoints($containerType, $contentType, $containerID, $contentID, $action);
+
+        //loads container's model
+        $this->loadModel($containerType);
+
+        //cases to populate with joint table
+        switch ($containerType . $contentType) {
+            case 'TeamsUsers':
+                $containerTable = $this->Teams;
+                $contentTable = $this->Teams->Users;
+                break;
+            case 'TeamsMaterials':
+                $containerTable = $this->Teams;
+                $contentTable = $this->Teams->Materials;
+                break;
+            case 'TeamsVehicles':
+                $containerTable = $this->Teams;
+                $contentTable = $this->Teams->Vehicles;
+                break;
+            case 'EventsTeams':
+                $containerTable = $this->Events;
+                $contentTable = $this->Events->Teams;
+                break;
+        }
+
+        //get container query object
+        $container = $containerTable->get($containerID, ['contain' => [$contentType]]);
+        //get content
+        $content = $contentTable->findById($contentID)->toArray();
+
+        //links or unlinks container and content
+        if ($action == 'add') {
+            $contentTable->link($container, $content);
+        } elseif ($action == 'remove') {
+            $contentTable->unlink($container, $content);
+        }
+
 
     }
 
